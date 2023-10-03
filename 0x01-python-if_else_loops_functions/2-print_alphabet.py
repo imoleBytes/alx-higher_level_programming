a5d21ccca7ac49c4c0b67f37de45ffb5ec8d24a1
imole <dremkay71@gmail.com>
2023-10-03 12:34:12 +0100

    task2: no mixed tabs and spaces

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -3,5 +3,5 @@
 for i in range(26):
     ascii_value = i + ord("a")
     alph = chr(ascii_value)
-    print(alph, end="")
+    print("{:s}".format(alph), end="")
     # print("{:s}".format(chr(char + ord("a"))), end="")
