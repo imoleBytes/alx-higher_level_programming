668f1902ec619b07eba86a45b05bbcc1e0e8a787
imole <dremkay71@gmail.com>
2023-10-03 12:36:56 +0100

    task2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 
 for i in range(26):
-    ascii_value = i + ord("a")
-    alph = chr(ascii_value)
-    print("{:s}".format(alph), end="")
-    # print("{:s}".format(chr(char + ord("a"))), end="")
+    print("{:s}".format(chr(i + ord("a"))), end="")
