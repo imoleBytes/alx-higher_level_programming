480cbd005ce0959c7a722a0f5b5146a13ec0d0c1
imole <dremkay71@gmail.com>
2023-10-06 16:49:27 +0100

    task3

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+    start = -1
+    end = len(my_list) * -1
+
+    while end <= start:
+        print(my_list[start])
+        start -= 1
