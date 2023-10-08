5052e3dacc911a36232823da892eef60dd92a1d0
imole <dremkay71@gmail.com>
2023-10-08 18:18:33 +0100

    task 6: more consice

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+'''
 def print_matrix_integer(matrix=[[]]):
     if len(matrix) == 1 and len(matrix[0]) == 0:
         return
@@ -8,4 +8,12 @@ def print_matrix_integer(matrix=[[]]):
             col = row[i]
             print("{:d} ".format(col), end="")
         print(row[-1], end="")
+        print()
+'''
+
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for col in row:
+            print("{:d}".format(col), end=" " if col != row[-1] else "")
         print()
