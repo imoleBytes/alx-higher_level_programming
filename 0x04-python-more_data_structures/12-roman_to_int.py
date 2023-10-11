fbf82cfaceb9d95e0d0594d519a23e5fa72d5864
imole <dremkay71@gmail.com>
2023-10-11 15:59:59 +0100

    task12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+    roman_values = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+    total = 0
+
+    prev = roman_values[roman_number[0]]
+
+    for i in roman_string:
+        total += roman_values[i]
+        if prev < roman_values[i]:
+            total -= prev * 2
+        prev = roman_values[i]
+    return total
