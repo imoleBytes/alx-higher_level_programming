fc91ed633a15aea8a5d0dbbbeb3b636ee917f668
imole <dremkay71@gmail.com>
2023-10-25 00:43:00 +0100

    task 0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """ this is Square class to create a blueprint for square shape"""
+    pass
+
+
+my_square = Square()
+print(type(my_square))
+print(my_square.__dict__)
