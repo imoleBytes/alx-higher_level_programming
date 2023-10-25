cd31d6668557bd1215b6969148f314e38e4e727a
imole <dremkay71@gmail.com>
2023-10-25 01:04:39 +0100

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""This module is all about creating a Square"""
+
+
+class Square:
+    """this is Square class to create a blueprint for square shape."""
+    def __init__(self, size):
+        """this special method initialize data"""
+        self.__size = size
