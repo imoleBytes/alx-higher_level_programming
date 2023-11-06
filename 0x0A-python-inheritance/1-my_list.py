d02427fd6f9f159865228389f49ecb6c8d8e90fc
imole <dremkay71@gmail.com>
2023-11-06 13:17:01 +0100

    tests

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -13,16 +13,5 @@ class MyList(list):
 
 if __name__ == "__main__":
     my_list = MyList()
-    # my_list.append(1)
-    # my_list.append(4)
-    # my_list.append(2)
-    # my_list.append(3)
-    # my_list.append(5)
-    # print(my_list)
-    # my_list.print_sorted()
-    # print(my_list)
-    # a = [1, 9, 4]
-    # my_list = MyList(a)
-    # my_list.print_sorted()
-    # no_lis.print_sorted()
     my_list.print_sorted()
+    li2.print_sorted()
