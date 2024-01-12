24a2ee0fbf780ef45f69c0ff5fa6a905515b3d6c
imole <dremkay71@gmail.com>
2024-01-12 13:51:28 +0100

    task 1: binary remved

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,7 +15,7 @@ def main():
                            passwd=sys.argv[2], db=sys.argv[3],
                            port=3306)
     cur = conn.cursor()
-    cur.execute("""SELECT * FROM states WHERE name LIKE BINARY 'N%'
+    cur.execute("""SELECT * FROM states WHERE name LIKE 'N%'
                 ORDER BY states.id""")
     results = cur.fetchall()
 
