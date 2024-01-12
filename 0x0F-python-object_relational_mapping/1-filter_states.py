1f45c5f57dcd1b11b3fde92730008d9eed8d0eef
imole <dremkay71@gmail.com>
2024-01-12 13:44:25 +0100

    task1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""This module contains the main function which connect to the database"""
+
+
+def printall(rows):
+    """prints all rows in results"""
+    for row in rows:
+        print(row)
+
+
+def main():
+    """Function connect to the database at localhost"""
+    conn = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3],
+                           port=3306)
+    cur = conn.cursor()
+    cur.execute("""SELECT * FROM states WHERE name LIKE BINARY 'N%'
+                ORDER BY states.id""")
+    results = cur.fetchall()
+
+    printall(results)
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    main()
