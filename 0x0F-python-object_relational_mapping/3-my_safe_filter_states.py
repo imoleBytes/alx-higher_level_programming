3070d09c07a5d8546a9c1b241c78301bd1005bba
imole <dremkay71@gmail.com>
2024-01-13 10:58:09 +0100

    task3: port added

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 
 """This module contains the main function which connect to the database"""
+import MySQLdb
+import sys
+
 
 def main():
     """Progrms statrts here!"""
     conn = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                           passwd=sys.argv[2], db=sys.argv[3])
+                           passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = conn.cursor()
     cur.execute("""SELCECT * FROM states WHERE name LIKE BINARY'%s'
                 ORDER BY states.id ASC""", (sys.argv[4], ))
@@ -22,6 +25,4 @@ def printall(rows):
 
 
 if __name__ == "__main__":
-    import MySQLdb
-    import sys
     main()
