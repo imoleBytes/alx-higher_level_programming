65e1141ba61f0e2e5c6e96f6b54b088e7754389a
imole <dremkay71@gmail.com>
2024-01-13 10:49:21 +0100

    task3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -6,49 +6,23 @@ import MySQLdb
 import sys
 
 
-# def main():
-#     """Progrms statrts here!"""
-#     conn = MySQLdb.connect(host="localhost", user=sys.argv[1],
-#  passwd=sys.argv[2], db=)
-
-# stmnt = """SELECT * FROM states WHERE name LIKE BINARY '{}'
-# ORDER BY states.id ASC"""
-
-def connect(host, user, passwd, database, port):
-    """connect to db"""
-    conn = MySQLdb.connect(host=host, user=user, passwd=passwd,
-                           db=database, port=port)
-    return conn
-
-
-def getCursor(c):
-    """gets cursor to work on"""
-    return c.cursor()
-
-
-def execute(cur, statement):
-    """executes statements"""
-    results = cur.execute(statement, (sys.argv[4], )).fetchall()
-    return results
+def main():
+    """Progrms statrts here!"""
+    conn = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3])
+    cur = conn.cursor()
+    cur.execute("""SELCECT * FROM states WHERE name LIKE %s
+                ORDER BY states.id ASC""", (sys.argv[4], ))
+    results = cur.fetchall()
+    printall(results)
+    cur.close()
+    conn.close()
 
 
 def printall(rows):
     """prints all results"""
     for row in rows:
         print(row)
-
-
-def main():
-    """main program starts here"""
-    db = connect("localhost", sys.argv[1], sys.argv[2], sys.argv[3], 3306)
-
-    cur = getCursor(db)
-    command = """SELECT * FROM states WHERE name LIKE %s
-                    ORDER BY states.id ASC"""
-    rows = execute(cur, command)
-    printall(rows)
-    cur.close()
-    db.close()
 
 
 if __name__ == "__main__":
