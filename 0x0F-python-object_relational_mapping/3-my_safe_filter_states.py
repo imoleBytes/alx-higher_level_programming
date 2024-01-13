ce9ff1487d435d31bb56f6e241fa6f36e2ff5171
imole <dremkay71@gmail.com>
2024-01-13 11:12:33 +0100

    task3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,39 +1,18 @@
 #!/usr/bin/python3
-"""List all states with names from argument
-from database hbtn_0e_0_usa safe from MySQL injections
-"""
+"""This module contains the main function which connect to the database"""
 import MySQLdb
 import sys
 
 
-def main():
-    """List all states with names from argument
-    from database hbnt_0e_0_usa safe from MySQL injections
-    """
-    MY_HOST = 'localhost'
-    MY_PORT = 3306
-    MY_USER = sys.argv[1]
-    MY_PASS = sys.argv[2]
-    MY_DB = sys.argv[3]
-    MY_ARG = sys.argv[4]
-    db = MySQLdb.connect(
-            host=MY_HOST,
-            port=MY_PORT,
-            user=MY_USER,
-            passwd=MY_PASS,
-            db=MY_DB
-    )
-    cur = db.cursor()
-    firstQuery = """SELECT id, name FROM states WHERE name """
-    secondQuery = """= %s ORDER BY id ASC"""
-    finalQuery = firstQuery + secondQuery
-    cur.execute(finalQuery, (MY_ARG,))
-    rows = cur.fetchall()
-    for row in rows:
+if __name__ == "__main__":
+    # main()
+    conn = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = conn.cursor()
+    cur.execute("""SELCECT * FROM states WHERE name LIKE BINARY '%s'
+                ORDER BY states.id ASC""", (sys.argv[4], ))
+    results = cur.fetchall()
+    for row in results:
         print(row)
     cur.close()
-    db.close()
-
-
-if __name__ == "__main__":
-    main()
+    conn.close()
