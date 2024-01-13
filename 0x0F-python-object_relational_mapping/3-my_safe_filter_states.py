d13ce862f040a9bf229e0cef6f303d2b77af7a37
imole <dremkay71@gmail.com>
2024-01-13 11:02:03 +0100

    task3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,28 +1,18 @@
 #!/usr/bin/python3
-
 """This module contains the main function which connect to the database"""
 import MySQLdb
 import sys
 
 
-def main():
-    """Progrms statrts here!"""
+if __name__ == "__main__":
+    # main()
     conn = MySQLdb.connect(host="localhost", user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = conn.cursor()
     cur.execute("""SELCECT * FROM states WHERE name LIKE BINARY'%s'
                 ORDER BY states.id ASC""", (sys.argv[4], ))
     results = cur.fetchall()
-    printall(results)
+    for row in results:
+        print(row)
     cur.close()
     conn.close()
-
-
-def printall(rows):
-    """prints all results"""
-    for row in rows:
-        print(row)
-
-
-if __name__ == "__main__":
-    main()
