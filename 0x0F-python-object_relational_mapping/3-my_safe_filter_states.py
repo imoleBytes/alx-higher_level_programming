46ae48d0391c76d086dcc5941e0ef3fbdfa65b47
imole <dremkay71@gmail.com>
2024-01-13 09:50:09 +0100

    task33: docstrings added

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+"""This module contains the main function which connect to the database"""
+
 import MySQLdb
 import sys
 
@@ -9,25 +11,30 @@ import sys
 
 
 def connect(host, user, passwd, database, port):
+    """connect to db"""
     conn = MySQLdb.connect(host, user, passwd, database, port)
     return conn
 
 
 def getCursor(c):
+    """gets cursor to work on"""
     return c.cursor()
 
 
 def execute(cur, statement):
+    """executes statements"""
     results = cur.execute(statement)
     return results
 
 
 def printall(rows):
+    """prints all results"""
     for row in rows:
         print(row)
 
 
 def main():
+    """mai program starts here"""
     db = connect("localhost", sys.argv[1], sys.argv[2], sys.argv[3], 3306)
 
     cur = getCursor(db)
