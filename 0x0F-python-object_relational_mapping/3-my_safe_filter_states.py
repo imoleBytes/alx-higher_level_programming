85b30960bcfc2615670a287a381dd811edff37c5
imole <dremkay71@gmail.com>
2024-01-13 12:11:37 +0100

    task3: specifaclly mentioned id, name instead of *

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = conn.cursor()
-    cur.execute("""SELCECT * FROM states WHERE name LIKE BINARY '%s'
+    cur.execute("""SELCECT id, name FROM states WHERE name LIKE BINARY '%s'
                 ORDER BY states.id ASC""", (sys.argv[4], ))
     results = cur.fetchall()
     for row in results:
