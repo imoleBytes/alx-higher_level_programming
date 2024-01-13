1b4979587ff6c79acd2b902320f9aae014326179
imole <dremkay71@gmail.com>
2024-01-13 14:12:45 +0100

    task5: pycodestyle does not accept bare except block

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -24,7 +24,7 @@ def main():
         for row in results[:-1]:
             print(row[0], end=", ")
         print(results[-1][0])
-    except:
+    except Exception:
         print()
     cur.close()
     conn.close()
