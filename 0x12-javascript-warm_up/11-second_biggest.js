4227e766cfbafbc6cc32ac77539328baf921a153
imoleFolio <dremkay71@gmail.com>
2024-02-12 16:14:29 +0100

    tak11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@
 let argv = process.argv.slice(2);
 const len = argv.length;
 
-if (len < 2) {
+if (len < 3) {
   console.log('0');
 } else {
   argv = argv.map(Number).sort(); // sort in ascending order
