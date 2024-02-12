f24959fb0c21585ec2a454eda470218d264513a9
imoleFolio <dremkay71@gmail.com>
2024-02-12 16:01:24 +0100

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,7 @@ let argv = process.argv.slice(2);
 const len = argv.length;
 
 if (len < 2) {
-  console.log(0);
+  console.log('0');
 } else {
   argv = argv.sort(); // sort in ascending order
   console.log(argv[len - 2]);
