01550c5622cb7e40d130f55dc11906e8abdbd33b
imoleFolio <dremkay71@gmail.com>
2024-02-12 15:00:19 +0100

    task8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,11 +4,11 @@ const argv = process.argv;
 let size = argv[2];
 
 if (isNaN(size)) {
-    console.log("Missing size");
+  console.log('Missing size');
 } else {
-    size = parseInt(size);
+  size = parseInt(size);
 
-    for (let i = 0; i < size; i++) {
-        console.log('X'.repeat(size));
-    }
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
 }
