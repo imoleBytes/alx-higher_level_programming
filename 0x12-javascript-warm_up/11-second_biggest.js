b035024e6737dcec9b8e4259f5a3d4e0729707c7
imoleFolio <dremkay71@gmail.com>
2024-02-12 16:16:57 +0100

    task11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,21 @@
 #!/usr/bin/node
 
-let argv = process.argv.slice(2);
-const len = argv.length;
+const argv = process.argv;
 
-if (len < 3) {
+if (argv.length <= 3) {
   console.log('0');
 } else {
-  argv = argv.map(Number).sort(); // sort in ascending order
-  console.log(argv[len - 2]);
+  const arr = argv.slice(2).map(Number);
+  const sec = arr.sort((a, b) => b - a)[1];
+  console.log(sec);
 }
+
+// let argv = process.argv.slice(2);
+// const len = argv.length;
+
+// if (len < 2) {
+//   console.log('0');
+// } else {
+//   argv = argv.map(Number).sort(); // sort in ascending order
+//   console.log(argv[len - 2]);
+// }
