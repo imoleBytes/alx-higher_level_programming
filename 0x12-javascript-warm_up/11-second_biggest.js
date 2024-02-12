8aec32e97cc140d45a39fbff0228ff8ca77e8a2a
imoleFolio <dremkay71@gmail.com>
2024-02-12 16:12:38 +0100

    ta11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-let argv = process.argv.slice(2).map(Number);
+let argv = process.argv.slice(2);
 const len = argv.length;
 
 if (len < 2) {
   console.log('0');
 } else {
-  argv = argv.sort(); // sort in ascending order
+  argv = argv.map(Number).sort(); // sort in ascending order
   console.log(argv[len - 2]);
 }
