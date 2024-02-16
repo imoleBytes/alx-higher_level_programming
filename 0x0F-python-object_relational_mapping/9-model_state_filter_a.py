ae1c95ee64419d918845dbe9918eabe3b9cf08f7
imoleFolio <dremkay71@gmail.com>
2024-02-16 16:38:17 +0100

    task9 o

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -23,8 +23,7 @@ def main():
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    results = session.query(State).filter(State.name
-                                          .like('%a%')).order_by(State.id)
+    results = session.query(State).filter(State.name.like('%a%'))
 
     for instance in results:
         print(instance.id, instance.name, sep=": ")
