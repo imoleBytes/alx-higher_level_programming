b5d15d75f0238e9c801697d4fdd1a47dc1443782
imoleFolio <dremkay71@gmail.com>
2024-02-17 15:25:48 +0100

    task14: ForeignKey imported

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -3,7 +3,7 @@
 City model, base objct created here
 """
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 
 Base = declarative_base()
