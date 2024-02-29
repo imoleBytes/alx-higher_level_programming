7c199778eb3ada033b3e48dac1caf50c701d75a4
imoleFolio <dremkay71@gmail.com>
2024-02-29 16:46:28 +0100

    task6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+contains find_peak function
+"""
+
+
+def find_peak(list_of_integers):
+    """
+    func finds the peak in a list
+    """
+    if not list_of_integers:
+        return None
+    for i in range(1, len(list_of_integers)):
+        if (list_of_integers[i] > list_of_integers[i - 1]) and \
+        (list_of_integers[i] > list_of_integers[i+1]):
+            return list_of_integers[i]
+    if list_of_integers[0] >= list_of_integers[1]:
+        return list_of_integers[0]
+    elif list_of_integers[-1] >= list_of_integers[-2]:
+        return list_of_integers[-1]
+
+
+print(find_peak([1, 2, 4, 6, 3]))
+print(find_peak([4, 2, 1, 2, 3, 1]))
+print(find_peak([2, 2, 2]))
+print(find_peak([]))
+print(find_peak([-2, -4, 2, 1]))
+print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))
